bf2fae0c48ff8d1704e4baedacc0cb5f88cf8a93
anshulkapoor <anshul.kapoor@africa.airtel.com>
2020-11-16 14:07:34 +0530

    decorator design pattern

---
+++ structural/src/main/java/com/designpatterns/structural/facade/JdbcFacadeDemo.java
@@ -0,0 +1,18 @@
+package com.designpatterns.structural.facade;
+
+public class JdbcFacadeDemo {
+
+    public void createTable() {
+
+    }
+
+    public void insertTable() {
+
+    }
+
+    public void getRecords() {
+
+    }
+
+    //Now calling this method from clients
+}
---
+++ structural/src/main/java/com/designpatterns/structural/facade/JdbcWithoutFacade.java
@@ -0,0 +1,14 @@
+package com.designpatterns.structural.facade;
+
+public class JdbcWithoutFacade {
+
+    public void withoutFacadeDemo() {
+        //code for getting singleton instance
+
+        //code for table creation
+        //code for creating connection, statement, execute statement
+
+        //code for getting getting records
+        //again using creating connection, statement, execute statement
+    }
+}
