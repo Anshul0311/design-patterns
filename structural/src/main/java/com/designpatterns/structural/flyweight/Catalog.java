10f40a9c11797c9165377a8eb1f4b64758f567f7
anshulkapoor <anshul.kapoor@africa.airtel.com>
2020-11-16 17:03:25 +0530

    flyweight design pattern

--- structural/src/main/java/com/designpatterns/structural/flyweight/Catalog.java
+++ structural/src/main/java/com/designpatterns/structural/flyweight/Catalog.java
@@ -8,6 +8,7 @@ public class Catalog {
 
     private Map<String,Item> items = new HashMap<>();
 
+    //factory method
     public Item lookup(String itemName) {
         if (!items.containsKey(itemName)) {
             items.put(itemName, new Item(itemName));
